fc863cd825ede22f6f703914359a65792e2932e7
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-01-27 22:10:01 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :lastname_em, presence: true
+  validates :firstname_em, presence: true
+  validates :lastname_katakana, presence: true
+  validates :firstname_katakana, presence: true
+  validates :birth_date, presence: true
+
 end
