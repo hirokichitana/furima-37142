ea77ebe974e5703f58df04afce7409f7705ac3d7
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-01-30 20:16:27 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ApplicationRecord
   validates :lastname_katakana, presence: true
   validates :firstname_katakana, presence: true
   validates :birth_date, presence: true
-  
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX}
+  
 end
