e585e3ff517bd7ac7820fad7581daddeb915b316
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-02-06 22:15:06 +0900

    ログインとログアウト状態で商品出品ページへの遷移の切り替え、バリデーションの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+
   def item_params
-    params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :lead_time_id, :price, :image)
+    params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :delivery_fee_id, :prefecture_id,
+                                 :lead_time_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,8 @@ class Item < ApplicationRecord
   belongs_to :lead_time
   has_one_attached :image
 
+  validates :image, presence: true
   validates :title, :explanation, presence: true
-
   validates :price, presence: true, numericality: { greater_than: 300, less_than: 9_999_999 }
 
   validates :category_id, numericality: { other_than: 1 }
