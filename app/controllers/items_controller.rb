774d4ce4547f73487c246ba51f8d76e91b25c64b
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-02-11 12:13:12 +0900

    itemコントローラーの重複記述部分のメソッド化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :contributor_confirmation, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -45,5 +44,9 @@ class ItemsController < ApplicationController
   def contributor_confirmation
     item = Item.find(params[:id])
     redirect_to root_path unless current_user == item.user
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
